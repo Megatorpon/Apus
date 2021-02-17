46c8a477c0b786a99a050141f5acb08ce62cdcdb
Megatorpon <ccppabloperez@gmail.com>
2021-02-17 23:41:14 +0100

    feat: dockerfile actualizado
    resolves #89

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,17 +2,34 @@ module.exports = function(grunt) {
 
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
+
 		run: {
-			tests: {
+			test: {
 				cmd: 'npm',
 				args: [
 				'run',
 				'test'
+				]
+			},
+
+			install: {
+				cmd: 'npm',
+				args: [
+					'install'
+				]
+			},
+
+			build: {
+				cmd: 'npm',
+				args: [
+					'build'
 				]
 			}
 		}
 	});
 
 	grunt.loadNpmTasks('grunt-run');
-	grunt.registerTask('testing', ['run']);
+	grunt.registerTask('build', ['run:build']);
+	grunt.registerTask('install', ['run:install']);
+	grunt.registerTask('test', ['run:test']);
 };
